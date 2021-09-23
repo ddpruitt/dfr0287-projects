c80d1ed2a58530ece1096a2f02d0ab872aa8d532
Darren Pruitt <darren.pruitt@gmail.com>
2021-09-23 09:35:45 -0500

    Very simple display of Humidity and Temerature.

--- Sensors/DHT11/DHT11 LCD01/src/main.cpp
+++ Sensors/DHT11/DHT11 LCD01/src/main.cpp
@@ -14,106 +14,86 @@ uint8_t menu_redraw_required = 0;
 
 #define DHT11_PIN 12 // Digital pin connected to the DHT sensor
 dht DHT;
-uint32_t delayMS;
+
+// min delay is ~1110
+uint32_t delayMS = 2000;
+
+void read_dht11(void)
+{
+  u8g2.setFont(u8g2_font_courR10_tr); // font height 14, width 10
+
+  uint32_t startRead = 0;
+  uint32_t stopRead = 0;
+
+  startRead = micros();
+  int chk = DHT.read11(DHT11_PIN);
+  stopRead = micros();
+
+  switch (chk)
+  {
+  case DHTLIB_OK:
+  // DISPLAY DATA
+    char humidityResult[5];
+    dtostrf(DHT.humidity, 4, 1, humidityResult);
+    u8g2.drawStr(0, 30, "Humidity:");
+    u8g2.drawStr(90, 30, humidityResult);
+
+    char temperatureResult[5];
+    dtostrf(DHT.temperature, 4, 1, temperatureResult);
+    u8g2.drawStr(0, 45, "Temp:");
+    u8g2.drawStr(90, 45, temperatureResult);
+    break;
+  case DHTLIB_ERROR_CHECKSUM:
+    u8g2.drawStr(0, 30, "Error - Checksum,");
+    break;
+  case DHTLIB_ERROR_TIMEOUT:
+    u8g2.drawStr(0, 30, "Error - Time out");
+    break;
+  case DHTLIB_ERROR_CONNECT:
+    u8g2.drawStr(0, 30, "Error - Connect");
+    break;
+  case DHTLIB_ERROR_ACK_L:
+    u8g2.drawStr(0, 30, "Error - Ack Low");
+    break;
+  case DHTLIB_ERROR_ACK_H:
+    u8g2.drawStr(0, 30, "Error - Ack High");
+    break;
+  default:
+    u8g2.drawStr(0, 30, "Error - Unknown");
+    break;
+  }
+
+  char result[8]; // Buffer big enough for 7-character float
+  dtostrf(stopRead - startRead, 6, 2, result);
+
+  u8g2.drawStr(0, 60, result);
+}
 
 void setup()
 {
-  Serial.begin(9600);
+  u8g2.begin();
+  u8g2.setDisplayRotation(U8G2_R2);
+  u8g2.setContrast(190);
 
-  Serial.println("DHT TEST PROGRAM ");
-  Serial.print("LIBRARY VERSION: ");
-  Serial.println(DHT_LIB_VERSION);
-  Serial.println();
-  Serial.println("Type,\tstatus,\t\tHumidity (%),\tTemperatur (C),\tT_Read (us)\tT_Cycle (us)\tT_Delay (us)");
-
-  // u8g2.begin();
-  // u8g2.setDisplayRotation(U8G2_R2);
-  // u8g2.setContrast(190);
-
-  // menu_redraw_required = 1;
-
+  menu_redraw_required = 1;
 }
-
-// min delay is ~1110
-int del = 1500;
-uint32_t startRead = 0;
-uint32_t stopRead = 0;
-uint32_t startCycle = 0;
-uint32_t stopCycle = 0;
-uint32_t tempCycle = 0;
 
 void loop()
 {
-  while (del > 0)
+  if (menu_redraw_required != 0)
   {
-    // READ DATA
-    Serial.print("DHT11, \t");
-
-    startRead = micros();
-    int chk = DHT.read11(DHT11_PIN);
-    stopRead = micros();
-
-    switch (chk)
+    u8g2.firstPage();
+    do
     {
-    case DHTLIB_OK:
-      Serial.print("OK,\t\t");
-      del -= 10;
-      break;
-    case DHTLIB_ERROR_CHECKSUM:
-      Serial.print("Checksum,\t");
-      break;
-    case DHTLIB_ERROR_TIMEOUT:
-      Serial.print("Time out,\t");
-      del += 10;
-      break;
-    case DHTLIB_ERROR_CONNECT:
-      Serial.print("Connect,\t");
-      del += 10;
-      break;
-    case DHTLIB_ERROR_ACK_L:
-      Serial.print("Ack Low,\t");
-      break;
-    case DHTLIB_ERROR_ACK_H:
-      Serial.print("Ack High,\t");
-      break;
-    default:
-      Serial.print("Unknown,\t");
-      break;
-    }
-    // DISPLAY DATA
-    Serial.print(DHT.humidity, 1);
-    Serial.print(",\t\t");
-    Serial.print(DHT.temperature, 1);
-    Serial.print(",\t\t");
-    Serial.print(stopRead - startRead);
-    Serial.print(",\t\t");
-    stopCycle = micros();
-    tempCycle = micros();
-    Serial.print(stopCycle - startCycle);
-    startCycle = tempCycle;
-    Serial.print(",\t\t");
-    Serial.print(del);
-    Serial.print("000");
-    Serial.println();
+      /* all graphics commands have to appear within the loop body. */
+      u8g2.setFont(u8g2_font_ncenB10_tr); // font height 15, width 16
+      u8g2.drawStr(0, 15, "DHT11 Sensor");
 
-    delay(del);
+      read_dht11();
+
+    } while (u8g2.nextPage());
+    menu_redraw_required = 0;
   }
-  while (1)
-    ;
-  //int update_page = 0;
-  // if (menu_redraw_required != 0)
-  // {
-  //   u8g2.firstPage();
-  //   do
-  //   {
-  //     /* all graphics commands have to appear within the loop body. */
-  //     u8g2.setFont(u8g2_font_ncenB14_tr);
-  //     u8g2.drawStr(0, 20, "DHT11 Sensor");
-
-  //     u8g2.setFont(u8g2_font_courR10_tr); // font height 14, width 10
-  //     delay(delayMS);
-
-  //   } while (u8g2.nextPage());
-  //   menu_redraw_required = 0;
-  // }
+  delay(delayMS);
+  menu_redraw_required = 1;
 }
