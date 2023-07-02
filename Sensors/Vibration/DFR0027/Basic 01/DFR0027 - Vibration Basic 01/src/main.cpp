8fe58e422febe0ee9593c2e867cc1e089cee5762
Darren Pruitt <darren.pruitt@gmail.com>
2023-07-02 15:08:37 -0500

    Added DFR0027 vibration sensor

---
+++ Sensors/Vibration/DFR0027/Basic 01/DFR0027 - Vibration Basic 01/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+#define SensorLED   13
+//Connect the sensor to digital Pin 3 which is Interrupts 1.
+#define SensorINPUT  3
+unsigned char state = 0;
+
+//Interrupts function 
+void blink() {
+    state++;
+}
+
+void setup() {
+  pinMode(SensorLED, OUTPUT);
+  pinMode(SensorINPUT, INPUT);
+  //Trigger the blink function when the falling edge is detected
+  attachInterrupt(1, blink, FALLING);
+}
+
+void loop() {
+  if (state != 0) {
+    state = 0;
+    digitalWrite(SensorLED, HIGH);
+    delay(500);
+  }
+  else
+    digitalWrite(SensorLED, LOW);
+}
+
