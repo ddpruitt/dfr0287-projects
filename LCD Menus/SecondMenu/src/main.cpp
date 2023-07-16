bff6d17f0bafcc397a83c07e1d3f163044fe4a6f
Darren Pruitt <darren.pruitt@gmail.com>
2023-07-15 19:21:50 -0500

    Moving the GEM Menu to 02 menu project.

--- LCD Menus/SecondMenu/src/main.cpp
+++ LCD Menus/SecondMenu/src/main.cpp
@@ -1,32 +1,60 @@
+/*
+  Basic menu example using GEM library.
+
+  Simple one page menu with one editable menu item associated with int variable, one with boolean variable,
+  and a button, pressing of which will result in int variable value printed to Serial monitor if boolean variable is set to true.
+
+  U8g2lib library is used to draw menu and to detect push-buttons presses.
+  
+  Additional info (including the breadboard view) available on GitHub:
+  https://github.com/Spirik/GEM
+  
+  This example code is in the public domain.
+*/
 #include <Arduino.h>
-#include <U8g2lib.h>
 
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
-#include <Wire.h>
-#endif
+#include <GEM_u8g2.h>
 
-// Please UNCOMMENT one of the contructor lines below
-// U8g2 Contructor List (Frame Buffer)
-// The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
+
+// Create an instance of the U8g2 library.
+// Use constructor that matches your setup (see https://github.com/olikraus/u8g2/wiki/u8g2setupcpp for details).
+// This instance is used to call all the subsequent U8g2 functions (internally from GEM library,
+// or manually in your sketch if it is required).
 // Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
 U8G2_ST7565_NHD_C12864_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/13, /* data=*/11, /* cs=*/10, /* dc=*/9, /* reset=*/8);
 
-#define KEY_NONE 0
-#define KEY_PREV 1
-#define KEY_NEXT 2
-#define KEY_SELECT 3
-#define KEY_BACK 4
-
-uint8_t uiKeyCodeFirst = KEY_NONE;
-uint8_t uiKeyCodeSecond = KEY_NONE;
-uint8_t uiKeyCode = KEY_NONE;
 
 int adc_key_in;
-int key = -1;
-int oldkey = -1;
+byte key = GEM_KEY_NONE;
+byte oldkey = GEM_KEY_NONE;
+
+/*
+menu.registerKeyPress(GEM_KEY_NONE); // Do nothing
+
+menu.registerKeyPress(GEM_KEY_UP); // Navigate up through the menu items list,
+                                   // select next value of the digit/char
+                                   // of editable variable, or previous option in select
+
+menu.registerKeyPress(GEM_KEY_RIGHT); // Navigate through the link to another (child) menu page,
+                                      // select next digit/char of editable variable,
+                                      // execute code associated with button
+
+menu.registerKeyPress(GEM_KEY_DOWN); // Navigate down through the menu items list,
+                                     // select previous value of the digit/char
+                                     // of editable variable, or next option in select
+
+menu.registerKeyPress(GEM_KEY_LEFT); // Navigate through the Back button to the previous menu page,
+                                     // select previous digit/char of editable variable
+
+menu.registerKeyPress(GEM_KEY_CANCEL); // Navigate to the previous (parent) menu page,
+                                       // exit edit mode without saving the variable,
+                                       // exit context loop if allowed within context's settings
+
+menu.registerKeyPress(GEM_KEY_OK); // Toggle boolean menu item, enter edit mode
+                                   // of the associated non-boolean variable,
+                                   // exit edit mode with saving the variable,
+                                   // execute code associated with button
+*/
 
 // Convert ADC value to key number
 //         4
@@ -34,20 +62,20 @@ int oldkey = -1;
 //   0 --  1 -- 3
 //         |
 //         2
-int get_key(unsigned int input)
+byte get_key(unsigned int input)
 {
   if (input < 100)
-    return 0;
+    return GEM_KEY_LEFT;
   else if (input < 300)
-    return 1;
+    return GEM_KEY_OK;
   else if (input < 500)
-    return 2;
+    return GEM_KEY_DOWN;
   else if (input < 700)
-    return 3;
+    return GEM_KEY_RIGHT;
   else if (input < 900)
-    return 4;
+    return GEM_KEY_UP;
   else
-    return -1;
+    return GEM_KEY_NONE;
 }
 
 void uiStep(void)
@@ -63,103 +91,76 @@ void uiStep(void)
     if (key != oldkey)
     {
       oldkey = key;
-      if (key >= 0)
-      {
-        //Serial.println(key);
-        if (key == 0)
-          uiKeyCodeFirst = KEY_BACK;
-        else if (key == 1)
-          uiKeyCodeFirst = KEY_SELECT;
-        else if (key == 2)
-          uiKeyCodeFirst = KEY_NEXT;
-        else if (key == 4)
-          uiKeyCodeFirst = KEY_PREV;
-        else
-          uiKeyCodeFirst = KEY_NONE;
-
-        uiKeyCode = uiKeyCodeFirst;
-      }
     }
   }
-  delay(100);
+  delay(150);
 }
 
-#define MENU_ITEMS 6
-char *menu_strings[MENU_ITEMS] = {"LCD12864 Shield", "www.DFRobot.com", "Temperature", "Humidity", "About", "OK"};
 
-uint8_t menu_current = 0;
-uint8_t menu_redraw_required = 0;
-uint8_t last_key_code = KEY_NONE;
+// Create variables that will be editable through the menu and assign them initial values
+int number = -512;
+boolean enablePrint = false;
 
-void drawMenu(void)
-{
-  uint8_t i, h;
-  u8g2_uint_t w, d;
+// Create two menu item objects of class GEMItem, linked to number and enablePrint variables 
+GEMItem menuItemInt("Number:", number);
+GEMItem menuItemBool("Enable print:", enablePrint);
 
-  u8g2.setFont(u8g_font_6x12); //4x6 5x7 5x8 6x10 6x12 6x13
-  u8g2.setFontRefHeightText();
-  u8g2.setFontPosTop();
+// Create menu button that will trigger printData() function. It will print value of our number variable
+// to Serial monitor if enablePrint is true. We will write (define) this function later. However, we should
+// forward-declare it in order to pass to GEMItem constructor
+void printData(); // Forward declaration
+GEMItem menuItemButton("Print", printData);
 
-  h = u8g2.getFontAscent() - u8g2.getFontDescent();
-  w = u8g2.getWidth();
-  for (i = 0; i < MENU_ITEMS; i++)
-  {
-    d = (w - u8g2.getStrWidth(menu_strings[i])) / 2;
-    u8g2.setDrawColor(1);
-    if (i == menu_current)
-    {
-      u8g2.drawBox(0, i * h + 1, w, h);
-      u8g2.setDrawColor(0);
-    }
-    u8g2.drawStr(d, i * h + 1, menu_strings[i]);
-  }
+// Create menu page object of class GEMPage. Menu page holds menu items (GEMItem) and represents menu level.
+// Menu can have multiple menu pages (linked to each other) with multiple menu items each
+GEMPage menuPageMain("Main Menu");
+
+// Create menu object of class GEM_u8g2. Supply its constructor with reference to u8g2 object we created earlier
+GEM_u8g2 menu(u8g2);
+
+void setupMenu() {
+  // Add menu items to menu page
+  menuPageMain.addMenuItem(menuItemInt);
+  menuPageMain.addMenuItem(menuItemBool);
+  menuPageMain.addMenuItem(menuItemButton);
+
+  // Add menu page to menu and set it as current
+  menu.setMenuPageCurrent(menuPageMain);
 }
 
-void updateMenu(void)
-{
-  switch (uiKeyCode)
-  {
-  case KEY_NEXT:
-    menu_current++;
-    if (menu_current >= MENU_ITEMS)
-      menu_current = 0;
-    menu_redraw_required = 1;
-    break;
-  case KEY_PREV:
-    if (menu_current == 0)
-      menu_current = MENU_ITEMS;
-    menu_current--;
-    menu_redraw_required = 1;
-    break;
-  }
-  uiKeyCode = KEY_NONE;
-}
+void setup() {
+  // Serial communication setup
+  Serial.begin(115200);
 
-void setup(void)
-{
   u8g2.begin();
-
-  // flip screen, if required
   u8g2.setDisplayRotation(U8G2_R2);
+  u8g2.setContrast(50);
 
-  // Testing shows that a contrast of 190 is acceptable.
-  u8g2.setContrast(190);
-
-  menu_redraw_required = 1; // force initial redraw
+  // Menu init, setup and draw
+  menu.init();
+  setupMenu();
+  menu.drawMenu();
 }
 
-void loop(void)
-{
-  uiStep();     // check for key press
-  updateMenu(); // update menu bar
 
-  if (menu_redraw_required != 0)
-  {
-    u8g2.firstPage();
-    do
-    {
-      drawMenu();
-    } while (u8g2.nextPage());
-    menu_redraw_required = 0;
+void loop() {
+
+  uiStep(); 
+  menu.registerKeyPress(key);
+  // If menu is ready to accept button press...
+  // if (menu.readyForKey()) {
+
+  //   menu.registerKeyPress();
+  // }
+}
+
+void printData() {
+  // If enablePrint flag is set to true (checkbox on screen is checked)...
+  if (enablePrint) {
+    // ...print the number to Serial
+    Serial.print("Number is: ");
+    Serial.println(number);
+  } else {
+    Serial.println("Printing is disabled, sorry:(");
   }
 }
