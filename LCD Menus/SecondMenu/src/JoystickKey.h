a5f354c610795e606a99d8e083116183ed77bf66
Darren Pruitt <darren.pruitt@gmail.com>
2023-07-15 20:12:11 -0500

    Added JoystickKey class h and cpp

---
+++ LCD Menus/SecondMenu/src/JoystickKey.h
@@ -0,0 +1,36 @@
+#ifndef _JOYSTICK_KEY_H
+#define _JOYSTICK_KEY_H
+
+#include <Arduino.h>
+#include <Gem_u8g2.h>
+
+class JoystickKey {
+    public:
+        JoystickKey();
+
+        byte trigger = GEM_KEY_NONE;
+        byte current = GEM_KEY_NONE;
+
+        void detect();
+
+    // Convert ADC value to key number
+    //          900
+    //           |
+    //   100 -- 300 -- 700
+    //           |
+    //          500
+    //
+
+    private:
+        int adc_key_in;
+
+        unsigned int _up    = 900;
+        unsigned int _right = 700;
+        unsigned int _down  = 500;
+        unsigned int _left  = 100;
+        unsigned int _ok    = 500;
+
+        byte get_key(unsigned int input);
+};
+
+#endif
