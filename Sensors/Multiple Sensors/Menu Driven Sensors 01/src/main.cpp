ac870a438a3c65c7c8e224a0b188ba3012ba266c
Darren Pruitt <darren.pruitt@gmail.com>
2023-07-17 20:26:20 -0500

    Added JoystickKey to Menu Driven Sensor 01

--- Sensors/Multiple Sensors/Menu Driven Sensors 01/src/main.cpp
+++ Sensors/Multiple Sensors/Menu Driven Sensors 01/src/main.cpp
@@ -11,10 +11,9 @@
   
   This example code is in the public domain.
 */
-#include <Arduino.h>
+#include "JoystickKey.h"
 
-#include <GEM_u8g2.h>
-
+JoystickKey joystickKey;
 
 // Create an instance of the U8g2 library.
 // Use constructor that matches your setup (see https://github.com/olikraus/u8g2/wiki/u8g2setupcpp for details).
@@ -22,79 +21,6 @@
 // or manually in your sketch if it is required).
 // Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
 U8G2_ST7565_NHD_C12864_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/13, /* data=*/11, /* cs=*/10, /* dc=*/9, /* reset=*/8);
-
-
-int adc_key_in;
-byte key = GEM_KEY_NONE;
-byte oldkey = GEM_KEY_NONE;
-
-/*
-menu.registerKeyPress(GEM_KEY_NONE); // Do nothing
-
-menu.registerKeyPress(GEM_KEY_UP); // Navigate up through the menu items list,
-                                   // select next value of the digit/char
-                                   // of editable variable, or previous option in select
-
-menu.registerKeyPress(GEM_KEY_RIGHT); // Navigate through the link to another (child) menu page,
-                                      // select next digit/char of editable variable,
-                                      // execute code associated with button
-
-menu.registerKeyPress(GEM_KEY_DOWN); // Navigate down through the menu items list,
-                                     // select previous value of the digit/char
-                                     // of editable variable, or next option in select
-
-menu.registerKeyPress(GEM_KEY_LEFT); // Navigate through the Back button to the previous menu page,
-                                     // select previous digit/char of editable variable
-
-menu.registerKeyPress(GEM_KEY_CANCEL); // Navigate to the previous (parent) menu page,
-                                       // exit edit mode without saving the variable,
-                                       // exit context loop if allowed within context's settings
-
-menu.registerKeyPress(GEM_KEY_OK); // Toggle boolean menu item, enter edit mode
-                                   // of the associated non-boolean variable,
-                                   // exit edit mode with saving the variable,
-                                   // execute code associated with button
-*/
-
-// Convert ADC value to key number
-//         4
-//         |
-//   0 --  1 -- 3
-//         |
-//         2
-byte get_key(unsigned int input)
-{
-  if (input < 100)
-    return GEM_KEY_LEFT;
-  else if (input < 300)
-    return GEM_KEY_OK;
-  else if (input < 500)
-    return GEM_KEY_DOWN;
-  else if (input < 700)
-    return GEM_KEY_RIGHT;
-  else if (input < 900)
-    return GEM_KEY_UP;
-  else
-    return GEM_KEY_NONE;
-}
-
-void uiStep(void)
-{
-
-  adc_key_in = analogRead(0); // read the value from the sensor
-  key = get_key(adc_key_in);  // convert into key press
-  if (key != oldkey)          // if keypress is detected
-  {
-    delay(50);                  // wait for debounce time
-    adc_key_in = analogRead(0); // read the value from the sensor
-    key = get_key(adc_key_in);  // convert into key press
-    if (key != oldkey)
-    {
-      oldkey = key;
-    }
-  }
-  delay(150);
-}
 
 
 // Create variables that will be editable through the menu and assign them initial values
@@ -130,7 +56,7 @@ void setupMenu() {
 
 void setup() {
   // Serial communication setup
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   u8g2.begin();
   u8g2.setDisplayRotation(U8G2_R2);
@@ -145,13 +71,13 @@ void setup() {
 
 void loop() {
 
-  uiStep(); 
-  menu.registerKeyPress(key);
+  //menu.registerKeyPress(key);
   // If menu is ready to accept button press...
-  // if (menu.readyForKey()) {
-
-  //   menu.registerKeyPress();
-  // }
+  if (menu.readyForKey()) {
+    joystickKey.detect();
+    menu.registerKeyPress(joystickKey.trigger);
+  }
+  delay(150);
 }
 
 void printData() {
