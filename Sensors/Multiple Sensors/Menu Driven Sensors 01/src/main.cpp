a7fb7f4e29d9300fd93e23f313a7155be9172ea1
Darren Pruitt <darren.pruitt@gmail.com>
2023-08-14 21:27:13 -0500

    DHT11 and TM35 submenu working.

--- Sensors/Multiple Sensors/Menu Driven Sensors 01/src/main.cpp
+++ Sensors/Multiple Sensors/Menu Driven Sensors 01/src/main.cpp
@@ -1,9 +1,23 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <DFRobot_DHT11.h>
 #include "JoystickKey.h"
 
 JoystickKey joystickKey;
 
 U8G2_ST7565_NHD_C12864_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/13, /* data=*/11, /* cs=*/10, /* dc=*/9, /* reset=*/8);
 uint8_t screen_redraw_required = 0;
+uint8_t temp_refresh_required = 0;
+
+int dht11_temp = 0;
+int dht11_hum = 0;
+uint16_t tm35_temp = 0;
+
+#define DHT11_PIN 12 // Digital pin connected to the DHT sensor
+DFRobot_DHT11 DHT;
+
+// min delay is ~1110
+uint32_t delayMS = 2000;
 
 void subMenu01(); // Forward declaration
 void subMenu01ContextEnter();
@@ -15,7 +29,9 @@ void subMenu02ContextEnter();
 void subMenu02ContextLoop();
 void subMenu02ContextExit();
 
-GEMItem menuItemButton01("Test 01", subMenu01);
+void read_dht11(void);
+
+GEMItem menuItemButton01("DHT11 & TM35", subMenu01);
 GEMItem menuItemButton02("Test 02", subMenu02);
 
 // Create menu page object of class GEMPage. Menu page holds menu items (GEMItem) and represents menu level.
@@ -38,26 +54,21 @@ void subMenu01ContextEnter()
   // Clear sreen
   u8g2.clear();
   screen_redraw_required = 1;
+  temp_refresh_required = 0;
   // Serial.println(F("Opening SubMenu 01"));
 }
 
 // Invoked every loop iteration
 void subMenu01ContextLoop()
 {
-  // Serial.print(F("screen_redraw_required: "));
-  // Serial.print(screen_redraw_required);
-
-  // Serial.print(F(", trigger: "));
-  // Serial.println(joystickKey.trigger);
-
   if (screen_redraw_required != 0)
   {
     u8g2.firstPage();
     do
     {
       /* all graphics commands have to appear within the loop body. */
-      u8g2.setFont(u8g2_font_ncenB10_tr); // font height 15, width 16
-      u8g2.drawStr(0, 15, "SubMenu 01");
+      // this next line needs a delay of ~1110ms to work
+      read_dht11();
 
       // Manual mode.
       // Check pressed keys and navigate through frames accordingly
@@ -70,24 +81,16 @@ void subMenu01ContextLoop()
         // Serial.println(F("Key OK - SubMenu Exit"));
         menu.context.exit();
         break;
-      case GEM_KEY_RIGHT:
-        u8g2.drawStr(0, 30, "Right");
-        break;
-      case GEM_KEY_LEFT:
-        u8g2.drawStr(0, 30, "Left");
-        break;
-      case GEM_KEY_UP:
-        u8g2.drawStr(0, 30, "Up");
-        break;
-      case GEM_KEY_DOWN:
-        u8g2.drawStr(0, 30, "Down");
+      default:
         break;
       }
 
     } while (u8g2.nextPage());
     screen_redraw_required = 0;
   }
+  //delay(1110);
   delay(100);
+
   screen_redraw_required = 1;
 }
 
@@ -209,4 +212,45 @@ void loop()
     menu.registerKeyPress(joystickKey.trigger);
   }
   delay(150);
+}
+
+
+void read_dht11(void)
+{
+  u8g2.setFont(u8g2_font_ncenB10_tr); // font height 15, width 16
+  u8g2.drawStr(0, 0, "DHT11 & TM35");
+
+  u8g2.setFont(u8g2_font_courR10_tr); // font height 14, width 10
+
+  DHT.read(DHT11_PIN);
+
+  if(temp_refresh_required <= 0)
+  {
+    dht11_temp = DHT.temperature;
+    dht11_hum = DHT.humidity;
+    tm35_temp = analogRead(A1); //Connect LM35 on Analog 1
+    temp_refresh_required = 15;
+  }
+  else
+  {
+    temp_refresh_required--;
+  }
+
+
+  char humidityResult[5];
+  dtostrf(dht11_hum, 4, 1, humidityResult);
+  u8g2.drawStr(0, 15, "Humidity:");
+  u8g2.drawStr(90, 15, humidityResult);
+
+  char temperatureResult[5];
+  dtostrf((1.8*dht11_temp + 32), 4, 1, temperatureResult);
+  u8g2.drawStr(0, 30, "DHT11:");
+  u8g2.drawStr(90, 30, temperatureResult);
+  
+  char tm35TempResult[8]; // Buffer big enough for 7-character float
+  dtostrf((1.8*((double) tm35_temp * (5/10.24)) + 32), 4, 1, tm35TempResult);
+  u8g2.drawStr(0, 45, "LM35:");
+  u8g2.drawStr(90, 45, tm35TempResult);
+
+  
 }
